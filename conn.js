3c531f6e7c6edd6b081b708d54e02a119d0e1b3b
Vaibhavdike93 <vaibhav.dike93@gmail.com>
2026-05-08 01:36:02 +0530

    update conn

--- conn.js
+++ conn.js
@@ -1,10 +1,10 @@
 var mysql = require("mysql");
 var util = require("util");
 var conn = mysql.createConnection({
-   host:"localhost",
-   user:"root",
-   password:"",
-   database:"my_portfolio" 
+   host:"bjtrxolvxasictsolc1x-mysql.services.clever-cloud.com",
+   user:"uvujqxojf3tdrlr4",
+   password:"hwMTCa8Z3iLX1nNuXUvs",
+   database:"bjtrxolvxasictsolc1x" 
 });
  var exe = util.promisify(conn.query).bind(conn);
 
